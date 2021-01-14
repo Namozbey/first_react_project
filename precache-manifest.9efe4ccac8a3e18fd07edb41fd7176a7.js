d4f0eea2ee1bf695d362e21c85f9b0e6181b9104
Namozbey <nostonayev@gmail.com>
2021-01-14 23:10:32 +0500

    Updates

---
+++ precache-manifest.9efe4ccac8a3e18fd07edb41fd7176a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16f4331b1a32f6bf9a8badf8f06c9a33",
+    "url": "/first_react_project/index.html"
+  },
+  {
+    "revision": "8b62cd83b0b9aa9ee46a",
+    "url": "/first_react_project/static/css/main.4fd05b83.chunk.css"
+  },
+  {
+    "revision": "639a2e56cc696064e95a",
+    "url": "/first_react_project/static/js/2.15f8bf67.chunk.js"
+  },
+  {
+    "revision": "37b5fb9fa73e0688add42527cf5813eb",
+    "url": "/first_react_project/static/js/2.15f8bf67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b62cd83b0b9aa9ee46a",
+    "url": "/first_react_project/static/js/main.8b03e86c.chunk.js"
+  },
+  {
+    "revision": "7f45059456cf57fd90b4",
+    "url": "/first_react_project/static/js/runtime-main.9d7c6bf3.js"
+  }
+]);
